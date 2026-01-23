78cbc4bc65e03def8a9bc149928308ecb8450643
Joe Ornstein <jornstein@uga.edu>
2026-01-23 09:52:01 -0500

    add raw/cleaned CLEA and ISO data

---
+++ _scripts/001_clean-data.R
@@ -0,0 +1,30 @@
+#' ---
+#' Clean CLEA and ISO datasets prior to record linkage
+#' ---
+
+library(tidyverse)
+library(here)
+
+## CLEA -----
+
+load(here(
+  '_data', 'raw', 'clea', 'clea_lc_20251015.RData'
+))
+
+clea <- clea_lc_20251015 |> 
+  filter(yr >= 1945) |> 
+  select(release, id, rg, ctr_n, ctr, yr, mn, sub, cst_n, cst) |> 
+  unique()
+
+# TODO: clean cst_n so that it can handle odd edge cases like Bolivia, where the name is split between two 
+
+## ISO codes -----
+
+iso <- read_csv(
+  here('_data', 'raw', 'ISO.csv'),
+  locale = locale(encoding = "latin1")
+)
+
+
+# save cleaned data to tmp/
+save(clea, iso, file = here('_data', 'tmp', 'cleaned.RData'))
